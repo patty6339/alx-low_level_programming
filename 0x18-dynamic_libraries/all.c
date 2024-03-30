3aa57c2fa8ef8c51fdc79e61c804ee03e5f9624b
Patrick <patty6339@gmail.com>
2024-03-30 06:19:02 -0700

    adding new file

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/**
+ * add - add two integers
+ * @a first int
+ * @b second int
+ * Return: addition 
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract two integers
+ * @a first int
+ * @b second int
+ * Return: subtracted final result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mod - modulus of two integers
+ * @a first int
+ * @b second int
+ * Return: final result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
+/**
+ * mul - multiply two integers
+ * @a first int
+ * @b second int
+ * Return: final result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divide two integers
+ * @a first int
+ * @b second int
+ * Return: final result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * mod - modulus of two integers
+ * @a first int
+ * @b second int
+ * Return: final result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
